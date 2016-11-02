070f220fe16971fbde8192fee9843e3d3616aa6b
Blake Scala and Dean Scelza <blake@>
2016-11-02 15:26:35 -0700

    add functionality for updating products

--- app/components/edit-product.js
+++ app/components/edit-product.js
@@ -2,5 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editProduct: false,
-  
+
+  actions : {
+    showEditForm() {
+      this.set('editProduct', true);
+    },
+
+    updateProduct(product) {
+      var params = {
+        name: this.get('name'),
+        cost: this.get('cost'),
+        image: this.get('image'),
+        category: this.get('category'),
+        clearance: this.get('clearance')
+      }
+      this.set('editProduct', false);
+      this.sendAction('updateProduct', product, params);
+      this.set('name', null);
+    }
+  }
+
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,11 +4,22 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('product');
   },
-  
+
   actions: {
     saveProduct(params) {
       var newProduct = this.store.createRecord('product', params);
       newProduct.save();
+      this.transitionTo('admin');
+    },
+
+    updateProduct(product, params) {
+      console.log('made it');
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          product.set(key, params[key]);
+        }
+      });
+      product.save();
       this.transitionTo('admin');
     }
   }
