bed6cf5d94071866c703082ec26965863be968a7
Blake Scala and Dean Scelza <blake@>
2016-11-02 10:42:38 -0700

    add functionality for adding new products

---
+++ app/components/new-product.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewProduct: false,
+
+  actions: {
+    showProductForm() {
+      this.set('addNewProduct', true);
+    },
+
+    saveProduct() {
+      var params = {
+        name: this.get('name'),
+        cost: this.get('cost'),
+        image: this.get('image'),
+        category: this.get('category'),
+        clearance: this.get('clearance'),
+        sales: 0,
+      };
+      if ((Ember.isEmpty(this.get('name'))) || (Ember.isEmpty(this.get('cost'))) || (Ember.isEmpty(this.get('image'))) || (Ember.isEmpty(this.get('category'))) ||
+      (Ember.isEmpty(this.get('clearance'))))
+      {
+        alert("You must complete this form!");
+      } else {
+        this.set('addNewProduct', false);
+        this.sendAction('saveProduct', params);
+      }
+    }
+  }
+});
