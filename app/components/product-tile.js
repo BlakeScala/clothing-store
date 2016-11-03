1621533fcc80419800c69045ac49f5b9ce239f7c
Blake Scala and Dean Scelza <dean@>
2016-11-03 15:47:02 -0700

    attempt implementing average rating

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  // averageRating: Ember.computed('product.comments', function() {
+  //   var sum = 0;
+  //
+  //   for(var i=0; i<product.comments.length; i++) {
+  //     sum += parseFloat(product.comments[i].rating);
+  //   }
+  //   var answer = sum / product.comments.length;
+  //   return answer;
+  // });
 });
